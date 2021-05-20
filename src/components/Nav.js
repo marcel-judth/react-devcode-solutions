f7293e669fc5a42c7001085960f9259c466fb4f4
Marcel Judth <42699984+marcel-judth@users.noreply.github.com>
2021-05-20 16:59:37 +0200

    test

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { HashLink as Link } from 'react-router-hash-link';
 import { motion } from 'framer-motion';
 import logo from '../img/logo.svg';
 
